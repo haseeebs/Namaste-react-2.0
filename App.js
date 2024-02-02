537621eab1cf1d4d7cbc36aa11f05860401f17df
haseeb hummo <haseebpersonalwork@gmail.com>
2024-02-02 19:45:52 +0530

    Completed Chapter 02 - Igniting our App.

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const div = React.createElement('div', { 'id': 'parent' },
         React.createElement('div', { 'id': 'child2' },
             [
                 React.createElement('h1', {}, 'hii m h1'),
-                React.createElement('h2', {}, 'this is h2')
+                React.createElement('h2', {}, "this is second child's h2")
             ])
     ]
 );
