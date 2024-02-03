b17632673f37beeb8a596a7389775e84d8e02a79
haseeb hummo <haseebpersonalwork@gmail.com>
2024-02-03 11:11:56 +0530

    feat: Implemented JSX syntax, functional components (Heading2, Heading3), and composed them within the Heading component in App.js.

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const div = React.createElement('div', { 'id': 'parent' },
-    [
-        React.createElement('div', { 'id': 'child' },
-            [
-                React.createElement('h1', {}, 'hii m h1'),
-                React.createElement('h2', {}, 'this is h2')
-            ]),
-        React.createElement('div', { 'id': 'child2' },
-            [
-                React.createElement('h1', {}, 'hii m h1'),
-                React.createElement('h2', {}, "this is second child's h2")
-            ])
-    ]
-);
+
+const Heading2 = () => <h1>This is heading 2</h1>
+const Heading3 = () => <h1>This is heading 3</h1>
+
+const Heading = () => {
+    return <div>
+        <Heading2 />
+        <Heading3 />
+    </div>
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(div);
+root.render(Heading());
