284937eb792d04ec8725d19e280a395cd3f552ae
haseeb hummo <haseebpersonalwork@gmail.com>
2024-02-07 10:57:27 +0530

    feat: Enhance Body.js with real Swiggy API data fetching and card styling.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
-import { restaurants } from "../../data";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchingData();
+  }, [])
+
+  const fetchingData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants); // here we have more restaurants how can we add them also
+    const restaurants = json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+    setListOfRestaurants(restaurants);
+  }
 
   const handleFilterTopRated = () => {
     const filteredList = listOfRestaurants.filter(
@@ -17,7 +28,7 @@ const Body = () => {
       <div className="search"></div>
 
       <div className="filter">
-        <button className="filter-btn" onClick={handleFilterTopRated}>
+        <button className="filter-btn buttons" onClick={handleFilterTopRated}>
           Top Rated Restaurants
         </button>
       </div>
