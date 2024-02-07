648cbc962b6b0ed33fc3f37e847d54441defac67
haseeb hummo <haseebpersonalwork@gmail.com>
2024-02-07 10:59:48 +0530

    feat : Implement show more button to fetch more restaurants from swiggy's api.

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     fetchingData();
@@ -23,6 +24,14 @@ const Body = () => {
     setListOfRestaurants(filteredList);
   };
 
+  const loadMoreRestaurants = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants); // here we have more restaurants how can we add them also
+    const restaurants = json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+    setListOfRestaurants(restaurant => [...restaurant, ...restaurants]);
+  };
+
   return (
     <div className="body">
       <div className="search"></div>
@@ -37,6 +46,7 @@ const Body = () => {
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} {...restaurant.info} />
         ))}
+        <button onClick={loadMoreRestaurants} className="buttons" disabled={showMore}>Show more</button>
       </div>
     </div>
   );
